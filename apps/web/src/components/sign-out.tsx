da89ec6e7f9b2bf38b0f82fa8222ca994e0229dd
Somya Bhatt <somyacu1@gmail.com>
2025-04-28 02:41:48 +0530

    chore: fixed lockfile

--- apps/web/src/components/sign-out.tsx
+++ apps/web/src/components/sign-out.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { authClient } from "@/server/auth/auth-client";
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/server/auth/auth-client";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
